23bb4209d2d4744f74caf722938c028c36cd5e7d
Demirichell <121028877+Demirichell@users.noreply.github.com>
2023-05-12 14:43:13 +0200

    fixed padding problem
    .

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
     <title> Boendemi</title>
+    <link rel="stylesheet" href="./css/main.css">
 </head>
 
 <body>
@@ -11,6 +12,16 @@
         <p>Contact</p>
         <p>Log in</p>
     </nav>
+    <header>
+        <div class="header-left"> 1</div>
+        <div class="header-right">
+            <form action="/action_page.php">
+                <label for="gsearch"></label>
+                <input type="search" id="gsearch" name="gsearch">
+                <input type="submit">
+            </form>
+        </div>
+    </header>
 </body>
 
 </html>
