d93f52e3ffbad390ee12229a9836008256043d2a
Demirichell <121028877+Demirichell@users.noreply.github.com>
2023-05-12 12:06:44 +0200

    setup
    .

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> Boendemi</title>
+</head>
+
+<body>
+    <nav>
+        <p>Over Ons</p>
+        <p>Contact</p>
+        <p>Log in</p>
+    </nav>
+</body>
+
+</html>
