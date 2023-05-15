e81898da76a13ebb21d2c934ced2982cfceb8adf
bleenie <117716131+bleenie@users.noreply.github.com>
2023-05-15 12:15:31 +0200

    login box startup

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/login.css">
+</head>
+
+<body>
+    <div class="login-box">
+        <div class="login-box-header">
+            <h3>Inloggen</h3>
+        </div>
+        <div class="login-box-content">
+            <form class="login-form">
+                <label for="email">Email</label><br>
+                <input type="email" id="email" name="email"><br>
+                <label for="password">Wachtwoord</label><br>
+                <input type="password" id="password" name="password"><br>
+                <a href="">Wachtwoord vergeten?</a><br>
+                Nog geen account? <span>Registreer hier!</span>
+                <div class="login-btn-box">
+                    <input class="login-btn" type="submit" value="Login">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
