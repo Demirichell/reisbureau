96cb5e68c363941fb2f830b6483431caec8939d0
Demirichell <121028877+Demirichell@users.noreply.github.com>
2023-05-16 10:10:07 +0200

    add icons

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <title> Boendemi</title>
     <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
 <body>
@@ -15,15 +16,44 @@
     <header>
         <div class="header-left"> 1</div>
         <div class="header-right">
-            <form action="/action_page.php">
+            <form class="searchform" action="/action_page.php">
                 <label for="gsearch"></label>
                 <input type="search" id="gsearch" name="gsearch">
                 <input type="submit">
             </form>
         </div>
     </header>
-    <section>
+    <section id="banner">
+
+        <div class="options">
+            <div class="searchoptions"><span class="material-symbols-outlined">
+                    sunny
+                </span> bestemmingen </div>
+            <div class="searchoptions"><span class="material-symbols-outlined">
+                    calendar_month
+                </span> vertrekdatum</div>
+            <div class="searchoptions"><span class="material-symbols-outlined">
+                    schedule
+                </span> Duur</div>
+            <div class="searchoptions"> <span class="material-symbols-outlined">
+                    flight
+                </span>vervoer</div>
+            <div class="searchbar"> search</div>
+        </div>
         <img src="img/travelmap.jpg" alt="">
+    </section>
+    <section id="icons">
+        <div class="iconssection"><img class="imgicon" src="./img/ice-cream.png" alt=""> </div>
+        <div class="iconssection"><img class="imgicon" src="./img/beach-ball.png" alt="">
+        </div>
+        <div class="iconssection"><img class="imgicon" src="./img/sunny.png" alt=""></div>
+        <div class="iconssection"><img class="imgicon" src="./img/flamingo.png" alt=""></div>
+    </section>
+
+    <section id="location">
+        <div>
+
+        </div>
     </section>
 </body>
 
