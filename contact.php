5bae830510c8697675a84a33911141b8295a6071
Demirichell <121028877+Demirichell@users.noreply.github.com>
2023-06-03 18:19:06 +0200

    .
    .

---
+++ contact.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> Boendemi</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
+
+<body>
+    <nav>
+        <p>Over Ons</p>
+        <p>Contact</p>
+        <p>Log in</p>
+    </nav>
+    <header>
+        <div class="header-left">
+            <img class="logo" src="./img/boendemi.png" alt="">
+        </div>
+        <div class="header-right">
+            <form class="searchform" action="/action_page.php">
+                <label for="gsearch"></label>
+                <input type="search" id="gsearch" name="gsearch">
+                <input type="submit">
+            </form>
+        </div>
+
+    </header>
+    <section id="banner">
+
+        <div class="options">
+            <div class="searchoptions"><span class="material-symbols-outlined">
+                    sunny
+                </span> bestemmingen </div>
+            <div class="searchoptions"><span class="material-symbols-outlined">
+                    calendar_month
+                </span> vertrekdatum</div>
+            <div class="searchoptions"><span class="material-symbols-outlined">
+                    schedule
+                </span> Duur</div>
+            <div class="searchoptions"> <span class="material-symbols-outlined">
+                    flight
+                </span>vervoer</div>
+            <div class="searchbar"> search</div>
+        </div>
+        <img src="img/travelmap.jpg" alt="">
+    </section>
+</body>
+
+</html>
