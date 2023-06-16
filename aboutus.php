8dbb70787b8e6ed64875ad6f9d97b3aa2c5c113a
Demirichell <121028877+Demirichell@users.noreply.github.com>
2023-06-16 13:25:55 +0200

    about us
    .

--- aboutus.php
+++ aboutus.php
@@ -3,6 +3,7 @@
 
 <head>
     <title> Boendemi</title>
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -31,27 +32,59 @@
             </form>
         </div>
     </header>
-    <section id="aboutusbanner">
-        <div class="aboutus">
-            <h1>About us</h1>
-        </div>
-        <img src="img/travelmap.jpg" alt="">
-    </section>
+    <img class="plane" src="img/eva-darron-oCdVtGFeDC0-unsplash.jpg" alt="">
     <section id="aboutuscontent">
         <div class="contentleft">
-            <img class="avatar" src="./img/avataaarsBo.png" alt="">
-            <img class="avatar" src="./img/avataaarsDemi.png" alt="">
+            <h2> Over ons</h2>
+            <h4>Welkom bij ons reisbureau! Wij zijn Bo en Demi, twee enthousiaste en avontuurlijke 21-jarigen die gepassioneerd zijn over reizen en het creëren van onvergetelijke ervaringen voor onze klanten. Als jonge reisexperts begrijpen we de behoeften en verlangens van onze generatie als het gaat om reizen. We geloven dat het ontdekken van nieuwe bestemmingen en culturen de sleutel is tot persoonlijke groei en levensverrijkende ervaringen.
+                Daarom streven we ernaar om reiservaringen te creëren die perfect aansluiten bij de interesses en verwachtingen van onze klanten. <br>
+                Onze filosofie is gebaseerd op het idee dat reizen niet alleen gaat om het bezoeken van toeristische attracties, maar ook om het beleven van authentieke momenten en het onderdompelen in de lokale cultuur.
+            </h4>
+            <div class="aboutusavatar">
+                <div class="contenavatar"> <img class="avatar" src="./img/avataaarsBo.png" alt=""></div>
+                <div class="bottemavatar">
+                    <h4>Bo Leenders</h4><br>
+                    <p></p>
+                </div>
+            </div>
+            <div class="aboutusavatar">
+                <div class="contenavatar"> <img class="avatar" src="./img/avataaarsDemi.png" alt=""></div>
+                <div class="bottemavatar">
+                    <h4> Demi de Groot</h4>
+                </div>
+            </div>
+
         </div>
+
         <div class="contentright">
-            <div class="aboutustext">
-                <h2> Waarom kiest u voor ons?</h2>
-                <h4>Welkom bij ons reisbureau! Wij zijn Bo en Demi, twee enthousiaste en avontuurlijke 21-jarigen die gepassioneerd zijn over reizen en het creëren van onvergetelijke ervaringen voor onze klanten. Als jonge reisexperts begrijpen we de behoeften en verlangens van onze generatie als het gaat om reizen. We geloven dat het ontdekken van nieuwe bestemmingen en culturen de sleutel is tot persoonlijke groei en levensverrijkende ervaringen.
-                    Daarom streven we ernaar om reiservaringen te creëren die perfect aansluiten bij de interesses en verwachtingen van onze klanten. <br>
-                    Onze filosofie is gebaseerd op het idee dat reizen niet alleen gaat om het bezoeken van toeristische attracties, maar ook om het beleven van authentieke momenten en het onderdompelen in de lokale cultuur.
-                    We vinden het belangrijk om de verborgen juweeltjes van elke bestemming te ontdekken en onze klanten te laten kennismaken met unieke ervaringen die ze altijd zullen koesteren. Als reisadviseurs staan we altijd klaar om onze klanten te helpen bij het plannen en organiseren van hun droomvakantie.
-                    Of het nu gaat om een ontspannen strandvakantie, een actief avontuur in de bergen of een culturele stedentrip, wij zorgen ervoor dat elk detail van de reis perfect wordt verzorgd.
-                    We luisteren aandachtig naar de wensen van onze klanten en stellen vervolgens een reisplan op maat samen dat aan al hun verwachtingen voldoet. Onze jeugdige energie en passie voor reizen zorgen ervoor dat we altijd op de hoogte zijn van de nieuwste trends en bestemmingen. We blijven constant leren en ontwikkelen om onze klanten te voorzien van de meest actuele en relevante informatie. Ons doel is om reizen toegankelijk en plezierig te maken voor iedereen,
-                    ongeacht het budget of de reisvoorkeuren. Dus, of je nu een relaxte strandliefhebber bent, een avontuurlijke ontdekkingsreiziger of een cultuurminnaar, Bo en Demi staan klaar om je te helpen jouw ultieme reiservaring te creëren. Samen zullen we de wereld ontdekken en herinneringen creëren die een leven lang meegaan! Neem vandaag nog contact met ons op en laat ons je helpen bij het plannen van jouw volgende avontuur!</h4>
+            <div class="waaromons">
+                <h5> waarom ons?</h5>
+
+                <p> <span class="material-symbols-outlined">
+                        done
+                    </span>laagste prijs garantie </p>
+                <p> <span class="material-symbols-outlined">
+                        done
+                    </span> Verzekerd op vakantie</p>
+                <p> <span class="material-symbols-outlined">
+                        done
+                    </span> Uit passie voor reizen </p>
+                <p> <span class="material-symbols-outlined">
+                        done
+                    </span> Beoordeeld door echte experts </p>
+                <p> <span class="material-symbols-outlined">
+                        done
+                    </span> Zonder zorgen op reis </p>
+                <p> <span class="material-symbols-outlined">
+                        done
+                    </span> Beste uit de test</p>
+                <p> <span class="material-symbols-outlined">
+                        done
+                    </span> Ik weet even niet wat ik hier moet neerzetten </p>
+            </div>
+
+            <div class="contact">
+                <h5> Contact?</h5>
             </div>
         </div>
 
