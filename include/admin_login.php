df1b2c4412fdfb6155d93c2ba7fa3e919799fccb
bleenie <117716131+bleenie@users.noreply.github.com>
2023-06-19 11:52:15 +0200

    login, registeren, admin login

---
+++ include/admin_login.php
@@ -0,0 +1,28 @@
+<?php
+include_once './connect.php';
+
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT * FROM admins WHERE email=:email AND password=:password");
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+    $value = $stmt->fetch();
+
+    if (isset($value['email'])) {
+        session_start();
+        $_SESSION['admin'] = array(
+            'email' => $value['email'],
+            'firstname' => $value['firstname'],
+            'lastname' => $value['lastname']
+        );
+        header("location:../index.php");
+    } else {
+        header("location: ../index.php?error=Invalid email or password");
+    }
+} else {
+    header("location: ../index.php");
+    exit();
+}
--- login.js
+++ login.js
@@ -1,5 +1,11 @@
 var overlay = document.getElementById("overlay");
 var login = document.getElementById("login-box");
+<<<<<<< Updated upstream
+=======
+var register = document.getElementById("register-box");
+var password = document.getElementById("password-box");
+var admin = document.getElementById("admin-box");
+>>>>>>> Stashed changes
 
 //zet de login overlay aan
 function LoginOverlayOn() {
@@ -11,4 +17,26 @@ function LoginOverlayOn() {
 function LoginOverlayOff() {
     overlay.style.display = "none";
     login.style.display = "none";
+<<<<<<< Updated upstream
 }
+=======
+    register.style.display = "none";
+    password.style.display = "none";
+    admin.style.display = "none";
+}
+
+function RegisterOverlayOn() {
+    login.style.display = "none"
+    register.style.display = "block";
+}
+
+function PasswordOverlayOn() {
+    login.style.display = "none"
+    password.style.display = "block";
+}
+
+function AdminOverlayOn() {
+    login.style.display = "none"
+    admin.style.display = "block";
+}
+>>>>>>> Stashed changes
